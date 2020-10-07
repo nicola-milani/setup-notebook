0252187ec357c9d04e599e576eea1b5c2f498ada
Nicola Milani <nicoweb313@gmail.com>
2020-10-08 00:53:40 +0200

    update lightdm theme

--- usr/local/bin/info.sh
+++ usr/local/bin/info.sh
@@ -4,3 +4,4 @@
 zenity --info --text "Ciao, ricordati che questo PC è di proprietà dell'Istituto Luigi Einaudi di Verona \n\nUsa questo PC solo per scopi didattici, per supporto scrivi a supporto-tecnico@einaudivr.it\n\nSalva i tuoi dati regolarmente su drive o supporto esterno" --height "200" --width "400"
 zenity --info --text "In alto a destra viene aggiunto il supporto alla lingua cinese\n\nUsa \"Tasto windows\" + \"barra spaziatrice\" per cambiare lingua rapidamente,\n\n" --height "200" --width "400"
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'it'), ('ibus', 'libpinyin')]"
+gsettings set org.gnome.desktop.a11y always-show-universal-access-status true
