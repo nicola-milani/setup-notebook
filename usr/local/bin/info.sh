16b79e58c4c8896d97280716f3bc4bc887839ae8
Nicola Milani <nicoweb313@gmail.com>
2020-10-08 14:16:33 +0200

    add space

--- usr/local/bin/info.sh
+++ usr/local/bin/info.sh
@@ -7,3 +7,4 @@ zenity --info --text "Ciao, ricordati che questo PC è di proprietà dell'Istitu
 zenity --info --text "In alto a destra viene aggiunto il supporto alla lingua cinese\n\nUsa \"Tasto windows\" + \"barra spaziatrice\" per cambiare lingua rapidamente,\n\n" --height "200" --width "400"
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'it'), ('ibus', 'libpinyin')]"
 gsettings set org.gnome.desktop.a11y always-show-universal-access-status true
+
--- usr/share/lightdm-webkit/themes/ein-theme/00_init.sh
+++ usr/share/lightdm-webkit/themes/ein-theme/00_init.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 cp /usr/share/lightdm-webkit/themes/ein-theme/message_center_standby.html /usr/share/lightdm-webkit/themes/ein-theme/message_center.html
+
