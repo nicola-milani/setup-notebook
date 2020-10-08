cdc65f8e09406634232e10592872aa8920742d8a
Nicola Milani <nicoweb313@gmail.com>
2020-10-08 18:24:59 +0200

    change order

--- usr/share/lightdm-webkit/themes/ein-theme/00_restore.sh
+++ usr/share/lightdm-webkit/themes/ein-theme/00_restore.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-rm /usr/share/lightdm-webkit/themes/ein-theme/message_center.html
-cp /usr/local/share/lightdm-msgs/message_center_default.html /usr/local/share/lightdm-msgs/message_center.html
-cp /usr/local/share/lightdm-msgs/message_center.html /usr/share/lightdm-webkit/themes/ein-theme/message_center.html
+ps -aux | grep gnome-shell | grep -v root
+if [ $? -eq 0 ]; then
+ cp /usr/local/share/lightdm-msgs/message_center_standby.html /usr/share/lightdm-webkit/themes/ein-theme/message_center.html
+ chmod 666 /usr/share/lightdm-webkit/themes/ein-theme/message_center.html
+ sed -i "s/USERNAME/$USER/g" /usr/share/lightdm-webkit/themes/ein-theme/message_center.html
+else
+ cp /usr/local/share/lightdm-msgs/message_center_default.html /usr/share/lightdm-webkit/themes/ein-theme/message_center.html
+ chmod 666 /usr/share/lightdm-webkit/themes/ein-theme/message_center.html
+fi
