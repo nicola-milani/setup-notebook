f00b3d2b06b6e29a337a29c135f7022c61cf73ca
nicola-milani <nicoweb313@gmail.com>
2020-10-10 09:42:26 +0200

    fix monitor

--- usr/bin/02-dual-monitor.sh
+++ usr/bin/02-dual-monitor.sh
@@ -6,7 +6,9 @@ secondary=$(xrandr | grep -w "connected" | cut -d " " -f1 | tail -1 )
 if [ $video_dev -gt 1 ]; then
    zenity --info --text "Tentativo di duplicazione schermo..." --height "200" --width "400"&
    pid=$! 
-   xrandr --output $secondary --same-as $primary
+   #xrandr --output $secondary --same-as $primary
+   xrandr --output $secondary --mode 1280x720
+   xrandr --output $primary --mode 1280x720
    sleep 2
    kill -9 $pid
 else
