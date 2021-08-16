fb8c0c74e37023c0b8ac48d7a1584169dd21a944
Nicola Milani <nicoweb313@gmail.com>
2021-08-16 19:06:17 +0200

    set vscode extension

---
+++ setVscode.sh
@@ -0,0 +1,16 @@
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-python.python
+code --install-extension janisdd.vscode-edit-csv
+code --install-extension mhutchie.git-graph
+code --install-extension ms-ceintl.vscode-language-pack-it
+code --install-extension mechatroner.rainbow-csv
+code --install-extension mongodb.mongodb-vscode
+code --install-extension redhat-developer.java
+code --install-extension redhat.vscode-yaml
+code --install-extension redhat.vscode-xml
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension ms-edgedevtools.vscode-edge-devtools
+code --install-extension ms-vscode.vscode-typescript-next
+code --install-extension foxundermoon.shell-format
+code --install-extension lmcarreiro.vscode-smart-column-indenter
