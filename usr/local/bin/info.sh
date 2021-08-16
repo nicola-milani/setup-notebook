a36c03393742b1fb6fb67660b8ea76ec51828e78
root <nicoweb313@gmail.com>
2021-08-16 15:55:27 +0200

    fix menu

--- usr/local/bin/info.sh
+++ usr/local/bin/info.sh
@@ -3,18 +3,26 @@
 
 #set menu
 gnome-extensions enable arc-menu@linxgem33.com
-gsettings set org.gnome.shell.extensions.arc-menu arc-menu-placement "DTD"
 #gsettings set org.gnome.shell.extensions.arc-menu menu-layout "Plasma"
 #gsettings set org.gnome.shell.extensions.arc-menu plasma-show-descriptions true
-#gsettings set org.gnome.shell.extensions.arc-menu plasma-selected-color 'rgb(229,165,10)'
+#gsettings set org.gnome.shell.extensions.arc-menu plasma-selected-color 'rgb(229,165,10)'#
 #gsettings set org.gnome.shell.extensions.arc-menu show-external-devices true
-#gsettings set org.gnome.shell.extensions.arc-menu extra-categories [(0, true), (1, true), (2, true), (3, false)]
-#<me-control-center.desktop'], ['Terminal', 'utilities-terminal-symbolic', 'org.gnome.Terminal.desktop'], ['Activities Overview', 'view-fullscreen-symbolic', 'ArcMenu_ActivitiesOverview']]
+#gsettings set org.gnome.shell.extensions.arc-menu extra-categories '[(0, true), (1, true), (2, true), (3, false)]'
 #gsettings set org.gnome.shell.extensions.arc-menu reload-theme true
 #gsettings set org.gnome.shell.extensions.arc-menu reload-theme false
-gsettings set org.gnome.shell.extensions.arc-menu menu-layout "Dashboard"
+
+#gsettings set org.gnome.shell.extensions.arc-menu menu-layout "Dashboard"
+#gsettings set org.gnome.shell.extensions.arc-menu reload-theme true
+#gsettings set org.gnome.shell.extensions.arc-menu reload-theme false
+
+
+gsettings set org.gnome.shell.extensions.arc-menu menu-layout 'Brisk'
+#gsettings set org.gnome.shell.extensions.arc-menu brisk-shortcuts-list "['Downloads', 'ArcMenu_Downloads', 'ArcMenu_Downloads', 'Settings', 'preferences-system-symbolic', 'gnome-control-center.desktop']"
+#gsettings set org.gnome.shell.extensions.arc-menu disable-recently-installed-apps false
+#gsettings set org.gnome.shell.extensions.arc-menu directory-shortcuts-list "[['Home', 'ArcMenu_Home', 'ArcMenu_Home'], ['Downloads', 'ArcMenu_Downloads', 'ArcMenu_Downloads']]"
 gsettings set org.gnome.shell.extensions.arc-menu reload-theme true
 gsettings set org.gnome.shell.extensions.arc-menu reload-theme false
+gsettings set org.gnome.shell.extensions.arc-menu arc-menu-placement "DTD"
 
 
 gsettings set org.gnome.shell.extensions.ding show-home false
@@ -31,4 +39,4 @@ gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'it'), ('ibus', 
 #zenity --info --text "In alto a destra viene aggiunto il supporto alla lingua cinese\n\nUsa \"Tasto windows\" + \"barra spaziatrice\" per cambiare lingua rapidamente\n\n" --height "200" --width "400"
 yad --info --image=/usr/share/icons/language-selector.png --text "In alto a destra viene aggiunto il supporto alla lingua cinese\n\nUsa \"Tasto windows\" + \"barra spaziatrice\" per cambiare lingua rapidamente\n\n "  --button=gtk-ok:1 --title="Note sulla lingua di inserimento" --fixed --borders=30 --escape-ok --center
 
-/usr/bin/02-dual-monitor.sh
+/usr/bin/02-dual-monitor.sh&
