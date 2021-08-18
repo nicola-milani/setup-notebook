32e8a06e73ad976f3b21ba9434abbe27647d06b2
Nicola Milani <nicoweb313@gmail.com>
2021-08-18 18:50:11 +0200

    set menu style

--- usr/local/bin/info.sh
+++ usr/local/bin/info.sh
@@ -24,13 +24,35 @@ gsettings set org.gnome.shell.extensions.arc-menu brisk-shortcuts-list "['Downlo
 gsettings set org.gnome.shell.extensions.arc-menu reload-theme true
 gsettings set org.gnome.shell.extensions.arc-menu reload-theme false
 gsettings set org.gnome.shell.extensions.arc-menu arc-menu-placement "DTD"
+gsettings set org.gnome.shell.extensions.arc-menu enable-large-icons true
+gsettings set org.gnome.shell.extensions.arc-menu disable-tooltips false
+gsettings set org.gnome.shell.extensions.arc-menu disable-recently-installed-apps true
+gsettings set org.gnome.shell.extensions.arc-menu enable-custom-arc-menu true
+gsettings set org.gnome.shell.extensions.arc-menu separator-color 'rgb(189,86,53)'
+gsettings set org.gnome.shell.extensions.arc-menu border-color 'rgb(189,86,53)'
+gsettings set org.gnome.shell.extensions.arc-menu highlight-color 'rgba(247,186,36,0.405405)'
+gsettings set org.gnome.shell.extensions.arc-menu highlight-foreground-color 'rgba(255,255,255,1)'
+gsettings set org.gnome.shell.extensions.arc-menu menu-arrow-size 0
+gsettings set org.gnome.shell.extensions.arc-menu menu-border-size 0
+gsettings set org.gnome.shell.extensions.arc-menu menu-color 'rgb(46,52,54)'
+gsettings set org.gnome.shell.extensions.arc-menu menu-corner-radius 8
+gsettings set org.gnome.shell.extensions.arc-menu menu-font-size 12
+gsettings set org.gnome.shell.extensions.arc-menu menu-foreground-color 'rgb(243,243,243)'
+gsettings set org.gnome.shell.extensions.arc-menu menu-margin 24
+gsettings set org.gnome.shell.extensions.arc-menu vert-separator true
+gsettings set org.gnome.shell.extensions.arc-menu menu-height 700
+gsettings set org.gnome.shell.extensions.arc-menu menu-width 360
 
+gsettings set org.gnome.shell.extensions.arc-menu reload-theme true
+gsettings set org.gnome.shell.extensions.arc-menu reload-theme false
 
 gsettings set org.gnome.shell.extensions.ding show-home false
 gsettings set org.gnome.shell.extensions.ding show-trash false
 gsettings set org.gnome.mutter center-new-windows true
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'libreoffice-writer.desktop', 'code_code.desktop', 'kazam.desktop', 'org.gnome.Screenshot.desktop', 'scratch-desktop.desktop', 'duplicate.desktop', 'termina-sessione.desktop']"
+
+
 #zenity --info --text "Ciao, ricordati che questo PC è di proprietà dell'Istituto Luigi Einaudi di Verona \n\nUsa questo PC solo per scopi didattici, per supporto scrivi a supporto-tecnico@einaudivr.it\n\nSalva i tuoi dati regolarmente su Google Drive o supporto usb esterno" --height "200" --width "400"
 yad --info --image=/usr/share/icons/googleLogo.png --text "Ciao, ricordati che questo PC è di proprietà dell'Istituto Luigi Einaudi di Verona \n\nUsa questo PC solo per scopi didattici, per supporto scrivi a supporto-tecnico@einaudivr.it\n\nSalva i tuoi dati regolarmente su Google Drive o supporto usb esterno "  --button=gtk-ok:1 --title="Note sul salvataggio dei dati" --fixed --borders=30 --escape-ok --center
 
